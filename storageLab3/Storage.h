2c42f2e0f6573dfb5f9f86deed18566fd27b15fd
kammarx <kammarufa6@gmail.com>
2021-12-14 18:17:04 +0500

    removeObj(),setObj() and others

--- storageLab3/Storage.h
+++ storageLab3/Storage.h
@@ -20,17 +20,59 @@ public:
 		if (size != 0) {
 			for (int i = 0; i < size; i++) {
 				storage2[i] = storage[i];
-				delete storage[i];
+				
 			}
 		}
 		storage2[size] = newobj;
-		storage = storage2;
 		size++;
+		delete[] storage;
+		storage = storage2;
+		
 	}
 
 	void deleteObj(int k) {
 		storage[k] = nullptr;
 	}
+
+	Item& getObj(int k) {
+		return *storage[k];
+	}
+
+	Item& getObjDel(int k) {
+		Item* temp = storage[k];
+		storage[k] = nullptr;
+		return *temp;
+	}
+
+	void setObj(int i, Item* newobj) {
+		storage[i] = newobj;
+	}
+
+	int getCount() {
+		return size;
+	}
+
+	void removeObj(int k) {
+		if (size != 0) {
+			Item** storage2 = new Item * [size - 1];
+
+			for (int i = 0; i < k; i++) {
+				storage2[i] = storage[i];
+			}
+			delete storage[k];
+			for (int j = k; j < size - 1; j++) {
+				storage2[j] = storage[j + 1];
+			}
+			delete[] storage;
+			size--;
+			storage = storage2;
+		}
+
+	}
+
+
+
+
 
 
 };
